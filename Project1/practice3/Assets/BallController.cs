9b69677c3f13a487a98d8804ad984aed670887d3
bktl4ever <amangeldi99@gmail.com>
2019-11-18 00:19:10 +0600

    added practice3

---
+++ Project1/practice3/Assets/BallController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float BallForceScale = 400;
+    public float BallInitalAngle = 33;
+    private Rigidbody RigidBody;
+    
+    void Start()
+    {
+        RigidBody = GetComponent<Rigidbody>();
+
+        Vector3 force = Quaternion.Euler(0, BallInitalAngle, 0) *
+            Vector3.forward * BallForceScale;
+
+        RigidBody.AddForce(force);
+    }
+}
