80728fc002d1bb96d0db6a6c3a95e6254b10c574
bktl4ever <amangeldi99@gmail.com>
2019-11-17 15:16:45 +0600

    update .gitignore

---
+++ Project1/practice1/Assets/BallController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float InputForceScale = 10;
+    private Rigidbody RigidBody;
+
+    void Start()
+    {
+        RigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float HorizontalAxis = Input.GetAxis("Horizontal");
+        float VerticalAxis = Input.GetAxis("Vertical");
+
+        Vector3 force = 
+            new Vector3(HorizontalAxis, 0, VerticalAxis) * InputForceScale;
+
+        RigidBody.AddForce(force);
+    }
+}
