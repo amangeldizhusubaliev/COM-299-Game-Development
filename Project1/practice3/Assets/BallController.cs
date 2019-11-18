8c71a8f4a32d3390b098352688d67819f0a13591
bktl4ever <amangeldi99@gmail.com>
2019-11-18 16:16:09 +0600

    added ball respawn sound and parameters

--- Project1/practice3/Assets/BallController.cs
+++ Project1/practice3/Assets/BallController.cs
@@ -3,10 +3,13 @@
 public class BallController : MonoBehaviour
 {
     public float BallForceScale = 400;
-    public float BallInitalAngle = 33;
+    private float BallInitalAngle;
 
+    public int LastPlayer = -1;
+    
     public AudioClip WallSound;
     public AudioClip PaddleSound;
+    public AudioClip BallOutOfBounds;
 
     private Rigidbody RigidBody;
     private AudioSource audioSource;
@@ -14,13 +17,28 @@ public class BallController : MonoBehaviour
     void Start()
     {
         RigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
+        if (LastPlayer != -1)
+        {
+            audioSource.PlayOneShot(BallOutOfBounds);
+        } else
+        {
+            LastPlayer = Random.Range(0, 1) < 0.5 ? 1 : 2;
+        }
+
+        if (LastPlayer == 1)
+        {
+            BallInitalAngle = Random.Range(-45, -135);
+        } else
+        {
+            BallInitalAngle = Random.Range(45, 135);
+        }
 
         Vector3 force = Quaternion.Euler(0, BallInitalAngle, 0) *
             Vector3.forward * BallForceScale;
 
         RigidBody.AddForce(force);
-
-        audioSource = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter(Collision collision)
--- Project1/practice3/Assets/GameController.cs
+++ Project1/practice3/Assets/GameController.cs
@@ -6,6 +6,7 @@ public class GameController : MonoBehaviour
     public GameObject BallTemplate;
     public Text FirstPlayerScore;
     public Text SecondPlayerScore;
+    private int LastPlayer = -1;
 
     private int FirstPlayerScoreCounter = 0;
     private int SecondPlayerScoreCounter = 0;
@@ -20,19 +21,24 @@ public class GameController : MonoBehaviour
 
             if (ball.transform.position.x < transform.position.x)
             {
+                LastPlayer = 2;
                 SecondPlayerScoreCounter++;
-                SecondPlayerScore.text = 
+                SecondPlayerScore.text =
                     SecondPlayerScoreCounter.ToString();
             }
             else
             {
+                LastPlayer = 1;
                 FirstPlayerScoreCounter++;
                 FirstPlayerScore.text =
                     FirstPlayerScoreCounter.ToString();
             }
 
+            ball.GetComponent<BallController>().LastPlayer = LastPlayer;
+
+            Instantiate(ball, new Vector3(0, 0.5f, 0), Quaternion.identity);
+
             Destroy(ball);
-            Instantiate(BallTemplate);
         }
     }
     
